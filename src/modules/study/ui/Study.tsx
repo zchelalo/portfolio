d08ee70ead316a28377466b530d7de85afb3e7f0
zchelalo <eduardosaavedra687@gmail.com>
2024-08-31 23:03:37 -0700

    add: studies section

--- src/modules/study/ui/Study.tsx
+++ src/modules/study/ui/Study.tsx
@@ -12,7 +12,33 @@ function Study() {
       id={Section.STUDIES}
     >
       <main>
-        <h1>Study</h1>
+        <div className='rounded bg text border-2 border-violet-900 flex flex-wrap justify-between items-center p-2 mt-4'>
+          <div className='flex flex-col'>
+            <h2 className='font-semibold'>CECyTES</h2>
+            <span>Programming Technician</span>
+          </div>
+          <span>
+            2018 - 2021
+          </span>
+        </div>
+        <div className='rounded bg text border-2 border-violet-900 flex flex-wrap justify-between items-center p-2 mt-4'>
+          <div className='flex flex-col'>
+            <h2 className='font-semibold'>Universidad Tecnológica de Nogales</h2>
+            <span>Higher University Technician in Information Technologies</span>
+          </div>
+          <span>
+            2021 - 2023
+          </span>
+        </div>
+        <div className='rounded bg text border-2 border-violet-900 flex flex-wrap justify-between items-center p-2 mt-4'>
+          <div className='flex flex-col'>
+            <h2 className='font-semibold'>Universidad Tecnológica de Nogales</h2>
+            <span>Engineer in Multiplatform Software Development and Management</span>
+          </div>
+          <span>
+            2023 - 2025
+          </span>
+        </div>
       </main>
     </SectionLayout>
   )
