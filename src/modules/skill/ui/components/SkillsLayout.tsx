576c814e667d9bbae539e9bacd070a06796083a0
zchelalo <eduardosaavedra687@gmail.com>
2024-09-02 00:02:28 -0700

    update: First letter to uppercase at SkillsLayout

--- src/modules/skill/ui/components/SkillsLayout.tsx
+++ src/modules/skill/ui/components/SkillsLayout.tsx
@@ -2,6 +2,8 @@ import { SkillEntity } from '@/modules/skill/domain/entity'
 
 import { SkillLevel } from '@/constants'
 
+import { firstLetterToUpperCase } from '@/utils/string'
+
 import { ButtonTechnology } from '@/components/ButtonTechnology'
 
 type SkillsLayoutProps = {
@@ -15,7 +17,7 @@ function SkillsLayout({
 }: SkillsLayoutProps) {
   return (
     <section className='w-full flex flex-col mt-4'>
-      <h2 className='text text-xl font-medium'>{level}</h2>
+      <h2 className='text text-xl font-medium'>{firstLetterToUpperCase(level)}</h2>
       <div className='w-full flex flex-wrap items-center'>
         {skills?.map(skill => (
           <ButtonTechnology key={skill.id} technology={skill.name} />
