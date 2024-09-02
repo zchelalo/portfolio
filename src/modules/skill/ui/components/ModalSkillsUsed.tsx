217032080a6b425ee4dacfe63b72211facc1e61c
zchelalo <eduardosaavedra687@gmail.com>
2024-09-02 01:33:57 -0700

    add: modal skills used in projects

---
+++ src/modules/skill/ui/components/ModalSkillsUsed.tsx
@@ -0,0 +1,59 @@
+import { SkillEntity } from '@/modules/skill/domain/entity'
+import { ProjectEntity } from '@/modules/project/domain/entity'
+
+import { firstLetterToUpperCase } from '@/utils/string'
+
+import { Modal } from '@/components/Modal'
+import { Button } from '@/components/Button'
+import { ProjectCard } from '@/components/ProjectCard'
+
+import { HiOutlineXMark } from 'react-icons/hi2'
+
+type ModalSkillsUsedProps = {
+  skill: SkillEntity
+  projects: ProjectEntity[]
+  setSelectedSkill: (skill: SkillEntity | undefined) => void
+  setProjects: (projects: ProjectEntity[]) => void
+  setModalIsOpen: (isOpen: boolean) => void
+}
+
+function ModalSkillsUsed({
+  skill,
+  projects,
+  setSelectedSkill,
+  setProjects,
+  setModalIsOpen
+}: ModalSkillsUsedProps) {
+  return (
+    <Modal>
+      <div className='w-2/3 max-h-[80vh] rounded bg text flex flex-col p-4'>
+        <header className='w-full flex flex-wrap justify-between items-center'>
+          <h3 className='text-lg font-medium'>
+            Projects that I have done with {firstLetterToUpperCase(skill.name)}
+          </h3>
+          <Button
+            type='button'
+            className='bg-hover-secondary'
+            onClick={() => {
+              setModalIsOpen(false)
+              setSelectedSkill(undefined)
+              setProjects([])
+            }}
+          >
+            <HiOutlineXMark />
+          </Button>
+        </header>
+        <main className='flex flex-col justify-center items-center'>
+          {projects.map((project) => (
+            <ProjectCard
+              key={project.id}
+              project={project}
+            />
+          ))}
+        </main>
+      </div>
+    </Modal>
+  )
+}
+
+export { ModalSkillsUsed }
--- src/modules/skill/ui/components/SkillsLayout.tsx
+++ src/modules/skill/ui/components/SkillsLayout.tsx
@@ -1,9 +1,15 @@
 import { SkillEntity } from '@/modules/skill/domain/entity'
+import { ProjectEntity } from '@/modules/project/domain/entity'
+import { MemoryRepository } from '@/modules/skill/infrastructure/repositories/memory'
+import { SkillUseCase } from '@/modules/skill/application/use_cases/skill'
+
+import { useState } from 'react'
 
 import { SkillLevel } from '@/constants'
 
 import { firstLetterToUpperCase } from '@/utils/string'
 
+import { ModalSkillsUsed } from '@/modules/skill/ui/components/ModalSkillsUsed'
 import { ButtonTechnology } from '@/components/ButtonTechnology'
 
 type SkillsLayoutProps = {
@@ -11,16 +17,49 @@ type SkillsLayoutProps = {
   skills: SkillEntity[]
 }
 
+const skillRepository = new MemoryRepository()
+const skillUseCase = new SkillUseCase(skillRepository)
+
 function SkillsLayout({
   level,
   skills
 }: SkillsLayoutProps) {
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+  const [selectedSkill, setSelectedSkill] = useState<SkillEntity>()
+  const [projects, setProjects] = useState<ProjectEntity[]>()
+
+  const fetchProjects = async (skill: SkillEntity) => {
+    if (!skill) return
+    const skillObtained = await skillUseCase.getSkillById(skill.id as string)
+
+    if (!skillObtained) return
+    if (!skillObtained.projects.length) return
+
+    setSelectedSkill(skillObtained.skill)
+    setProjects(skillObtained.projects)
+    setModalIsOpen(true)
+  }
+
   return (
     <section className='w-full flex flex-col mt-4'>
+      {modalIsOpen && selectedSkill && projects ? (
+        <ModalSkillsUsed
+          skill={selectedSkill}
+          projects={projects}
+          setSelectedSkill={setSelectedSkill}
+          setProjects={setProjects}
+          setModalIsOpen={setModalIsOpen}
+        />
+      ) : undefined}
+
       <h2 className='text text-xl font-medium'>{firstLetterToUpperCase(level)}</h2>
       <div className='w-full flex flex-wrap items-center'>
-        {skills?.map(skill => (
-          <ButtonTechnology key={skill.id} technology={skill.name} />
+        {skills.map(skill => (
+          <ButtonTechnology
+            key={skill.id}
+            technology={skill.name}
+            onClick={() => fetchProjects(skill)}
+          />
         ))}
       </div>
     </section>
