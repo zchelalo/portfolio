27799d704f034767bc01154a0d9f5772b51e36b9
zchelalo <eduardosaavedra687@gmail.com>
2025-01-14 18:13:23 -0700

    update: title size at phone

--- src/components/SectionLayout/index.tsx
+++ src/components/SectionLayout/index.tsx
@@ -18,10 +18,10 @@ function SectionLayout({
     <section id={id} className='w-full flex flex-col justify-center mt-24'>
 
       <header className='flex items-center'>
-        <span className='text-2xl text mr-2'>
+        <span className='text-xl sm:text-2xl text mr-2'>
           {icon({ title })}
         </span>
-        <h1 className='text-2xl text font-medium'>{title}</h1>
+        <h1 className='text-xl sm:text-2xl text font-medium'>{title}</h1>
       </header>
 
       {children}
