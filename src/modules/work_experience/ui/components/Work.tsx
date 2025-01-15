e45598d0c5a805dbcc8a23acb559179ed0eec51f
zchelalo <eduardosaavedra687@gmail.com>
2025-01-14 18:23:39 -0700

    add: work experiences responsive

--- src/modules/work_experience/ui/components/Work.tsx
+++ src/modules/work_experience/ui/components/Work.tsx
@@ -16,8 +16,8 @@ function Work({
   return (
     <>
       <hr className='mt-4 border-violet-900' />
-      <article className='w-full flex rounded text mt-4'>
-        <header className='w-2/6 flex flex-col justify-between text-pretty'>
+      <article className='w-full sm:flex rounded text mt-4'>
+        <header className='w-full sm:w-2/6 flex flex-col justify-between text-pretty'>
           <div>
             <h2 className='text-lg font-medium'>{workExperience.company}</h2>
             <span>{moment(workExperience.startDate).utc(true).format('MMMM YYYY')} - {workExperience.endDate ? moment(workExperience.endDate).utc(true).format('MMMM YYYY') : 'Present'}</span>
@@ -26,7 +26,7 @@ function Work({
             {workExperience.position}
           </span>
         </header>
-        <main className='flex flex-col w-4/6 text-pretty pl-2'>
+        <main className='flex flex-col w-full text-pretty sm:w-4/6 sm:pl-2 mt-4 sm:mt-0'>
           <p>
             {workExperience.description}
           </p>
