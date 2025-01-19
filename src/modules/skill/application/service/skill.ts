9d7ce29acf5f7220cacf745cc74bc7463e1de217
zchelalo <eduardosaavedra687@gmail.com>
2025-01-19 15:20:30 -0700

    add: translations to skill alerts

--- src/modules/skill/application/service/skill.ts
+++ src/modules/skill/application/service/skill.ts
@@ -3,6 +3,9 @@ import { ProjectEntity } from '@/modules/project/domain/entity'
 import { SkillUseCase } from '@/modules/skill/application/use_cases/skill'
 import { paginationSchema } from '@/modules/skill/application/schemas/skill'
 
+import { NamespaceLanguage } from '@/constants'
+
+import { t } from 'i18next'
 import { toast } from 'sonner'
 
 export class SkillService {
@@ -16,17 +19,17 @@ export class SkillService {
     paginationSchema.parse({ offset, limit })
 
     if (!skill) {
-      toast.info('No skill selected')
+      toast.info(t(`${NamespaceLanguage.COMMON}:alerts.skills.no_selected`))
       return null
     }
     const skillObtained = await this.skillUseCase.getSkillById(skill.id as string, offset, limit)
 
     if (!skillObtained) {
-      toast.error('Skill not found')
+      toast.error(t(`${NamespaceLanguage.COMMON}:alerts.skills.not_found`))
       return null
     }
     if (!skillObtained.projects.length) {
-      toast.info('No projects to show')
+      toast.info(t(`${NamespaceLanguage.COMMON}:alerts.projects.no_projects_to_show`))
       return null
     }
 
